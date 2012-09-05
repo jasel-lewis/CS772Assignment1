4becb42ebf733c7f76cacacefc28227324402b61
jasel <jasel@10.0.0.2>
2012-09-05 17:43:43 -0400

    Initial commit

---
+++ CS772Assignment1Part1/src/com/jasel/cs772/assignment1/part1/part1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jasel.cs772.assignment1.part1;
+
+/**
+ * @author jasel
+ *
+ */
+public class part1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
